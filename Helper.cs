a3427b03f2fca8e9d98a694328df704fa77c92ec
Fan Yu <yufan@dev-yufan.visual-2000.cn>
2012-08-16 15:31:15 +0800

    first commit

---
+++ Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cassini
+{
+    public static class Helper
+    {
+        public static void PrintMessage(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
